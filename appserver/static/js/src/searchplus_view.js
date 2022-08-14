0fc295390e70018338a745a0373d50ea52239f31
Mohammed Latif <alatif113@gmail.com>
2022-08-14 13:48:00 -0400

    added edit search button

--- appserver/static/js/src/searchplus_view.js
+++ appserver/static/js/src/searchplus_view.js
@@ -33,13 +33,14 @@ define([
 		updateView: function (viz, data) {
 			viz.$el.html('');
 			data.forEach(function (row) {
-                let SKIPPED_DRILLDOWN = `/app/searchplus/search?q=search%20` + encodeURIComponent(`index=_internal sourcetype=scheduler status=skipped savedsearch_name="${row.title}"`) + `&earliest=-7d&latest=now`;
-                let RUN_TIME_DRILLDOWN = `/app/searchplus/search?q=search%20` + encodeURIComponent(`index=_internal sourcetype=scheduler run_time=* savedsearch_name="${row.title}"`) + `&earliest=-7d&latest=now`;
-                let MEM_USED_DRILLDOWN = `/app/searchplus/search?q=search%20` + encodeURIComponent(`index=_introspection sourcetype=splunk_resource_usage data.search_props.sid::* data.search_props.mode!=RT data.search_props.user!="splunk-system-user" data.search_props.label="${row.title}" | rename data.search_props.label as savedsearch_name data.mem_used as mem_used`) + `&earliest=-7d&latest=now`;
-                let SCAN_COUNT_DRILLDOWN = `/app/searchplus/search?q=search%20` + encodeURIComponent(`index=_audit sourcetype=audittrail search_id=* action=search savedsearch_name="${row.title}"`) + `&earliest=-7d&latest=now`;
-                let RESULT_COUNT_DRILLDOWN = `/app/searchplus/search?q=search%20` + encodeURIComponent(`index=_internal sourcetype=scheduler result_count=* savedsearch_name="${row.title}"`) + `&earliest=-7d&latest=now`;
+                let SEARCH_PREFIX = '/app/searchplus/search?q=search%20';
+                let SKIPPED_DRILLDOWN = SEARCH_PREFIX + encodeURIComponent(`index=_internal sourcetype=scheduler status=skipped savedsearch_name="${row.title}"`) + `&earliest=-7d&latest=now`;
+                let RUN_TIME_DRILLDOWN = SEARCH_PREFIX + encodeURIComponent(`index=_internal sourcetype=scheduler run_time=* savedsearch_name="${row.title}"`) + `&earliest=-7d&latest=now`;
+                let MEM_USED_DRILLDOWN = SEARCH_PREFIX + encodeURIComponent(`index=_introspection sourcetype=splunk_resource_usage data.search_props.sid::* data.search_props.mode!=RT data.search_props.user!="splunk-system-user" data.search_props.label="${row.title}" | rename data.search_props.label as savedsearch_name data.mem_used as mem_used`) + `&earliest=-7d&latest=now`;
+                let SCAN_COUNT_DRILLDOWN = SEARCH_PREFIX + encodeURIComponent(`index=_audit sourcetype=audittrail search_id=* action=search savedsearch_name="${row.title}"`) + `&earliest=-7d&latest=now`;
+                let RESULT_COUNT_DRILLDOWN = SEARCH_PREFIX + encodeURIComponent(`index=_internal sourcetype=scheduler result_count=* savedsearch_name="${row.title}"`) + `&earliest=-7d&latest=now`;
                 let SEARCH_DRILLDOWN = `/app/${row.app}/search?q=${(/^\s*\|/.test(row.search)) ? '' : 'search%20'}${encodeURIComponent(row.search)}&earliest=${row.earliest || '-24h'}&latest=${row.latest || 'now'}`;
-                let EDIT_URL = `/manager/searchplus/saved/searches?app=${row.app}&count=10&offset=0&itemType=&owner=${row.owner}&search=%22${encodeURIComponent(row.title)}%22'`;
+                let EDIT_URL = `/manager/searchplus/saved/searches?app=${row.app}&count=10&offset=0&itemType=&owner=${row.owner}&search=%22${encodeURIComponent(row.title)}%22`;
                 
                 let $markup = $(`
                     <li>
@@ -54,11 +55,14 @@ define([
                             <div><i class="icon icon-${row.status}"></i>${row.status}</div>
                         </div>
                         <div class="search-details">
-                            <nav role="tablist">
-                                <a href="#" aria-controls="general" role="tab" class="selected">General</a>
-                                <a href="#" aria-controls="attributes" role="tab">Attributes</a>
-                                <a href="#" aria-controls="statistics" role="tab">Statistics</a>
-                            </nav>
+                            <div class="nav-container">
+                                <nav role="tablist">
+                                    <a href="#" aria-controls="general" role="tab" class="selected">General</a>
+                                    <a href="#" aria-controls="attributes" role="tab">Attributes</a>
+                                    <a href="#" aria-controls="statistics" role="tab">Statistics</a>
+                                </nav>
+                                <a href="${EDIT_URL}" class="btn-edit-search btn-light"><i class="icon icon-external"></i>Edit Search</a>
+                            </div>
                             <div class="search-details-content">
                                 <section id="general" role="tabpanel" class="selected">
                                     <div class="query-container">
@@ -113,27 +117,27 @@ define([
                                     <div class="stat-${row.skipped_status || 'green'}">
                                         <div class="stat-label">Skipped Percent</div>
                                         <div class="stat-value">${row.skipped || 'N/A'}</div>
-                                        <a class="stat-drilldown" target="_blank" href="${SKIPPED_DRILLDOWN}"><i class="icon icon-external right-align"></i></a>
+                                        <a class="stat-drilldown btn-light" target="_blank" href="${SKIPPED_DRILLDOWN}"><i class="icon icon-external right-align"></i></a>
                                     </div>
                                     <div class="stat-${row.run_time_status || 'green'}">
                                         <div class="stat-label">Average Run Time</div>
                                         <div class="stat-value">${row.run_time || 'N/A'}</div>
-                                        <a class="stat-drilldown" target="_blank" href="${RUN_TIME_DRILLDOWN}"><i class="icon icon-external right-align"></i></a>
+                                        <a class="stat-drilldown btn-light" target="_blank" href="${RUN_TIME_DRILLDOWN}"><i class="icon icon-external right-align"></i></a>
                                     </div>
                                     <div class="stat-${row.mem_used_status || 'green'}">
                                         <div class="stat-label">Average Memory Used</div>
                                         <div class="stat-value">${row.mem_used || 'N/A'}</div>
-                                        <a class="stat-drilldown" target="_blank" href="${MEM_USED_DRILLDOWN}"><i class="icon icon-external right-align"></i></a>
+                                        <a class="stat-drilldown btn-light" target="_blank" href="${MEM_USED_DRILLDOWN}"><i class="icon icon-external right-align"></i></a>
                                     </div>
                                     <div class="stat-${row.scan_count_status || 'green'}">
                                         <div class="stat-label">Average Events Scanned</div>
                                         <div class="stat-value">${row.scan_count || 'N/A'}</div>
-                                        <a class="stat-drilldown" target="_blank" href="${SCAN_COUNT_DRILLDOWN}"><i class="icon icon-external right-align"></i></a>
+                                        <a class="stat-drilldown btn-light" target="_blank" href="${SCAN_COUNT_DRILLDOWN}"><i class="icon icon-external right-align"></i></a>
                                     </div>
                                     <div class="stat-${row.result_count_status || 'green'}">
                                         <div class="stat-label">Average Result Count</div>
                                         <div class="stat-value">${row.result_count || 'N/A'}</div>
-                                        <a class="stat-drilldown" target="_blank" href="${RESULT_COUNT_DRILLDOWN}"><i class="icon icon-external right-align"></i></a>
+                                        <a class="stat-drilldown btn-light" target="_blank" href="${RESULT_COUNT_DRILLDOWN}"><i class="icon icon-external right-align"></i></a>
                                     </div>
                                 </section>
                             </div>
